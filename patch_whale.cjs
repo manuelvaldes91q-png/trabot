a4a51d5bf6f1ba783bdfe7832d64cc925d0981a2
manuelvaldes91q-png <manuel.valdes91q@gmail.com>
2026-07-18 06:09:06 -0400

    refactor: cleanup build scripts and fix HTML structure
    Remove obsolete automation scripts and correct the Chart.js script tag inclusion in index.html.

--- patch_whale.cjs
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('index.html', 'utf-8');
-
-const regex = /function openWhaleMap\(mint, symbol\) \{[\s\S]*?\}\)[\s\S]*?\}/;
-if (regex.test(content)) {
-  console.log("Matched openWhaleMap");
-} else {
-  console.log("Did not match");
-}
